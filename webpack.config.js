b31e69bb5a4281e17d0d4351248ecdf1ddef4387
Ferdie <ferdie@yard.nl>
2017-06-05 12:42:05 +0200

    added webpack build for js and scss

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+	filename: "drawerrr.css"
+});
+
+module.exports = {
+ entry: [
+ 	'./src/js/drawerr.js',
+    './src/scss/drawerr.scss'
+ ],
+ output: {
+  filename: 'drawerr.min.js',
+  path: path.resolve( __dirname, 'dist' )
+ },
+ module: {
+	 rules: [{
+		 test: /\.scss$/,
+		 use: extractSass.extract({
+			 use: [{
+				 loader: "css-loader"
+			 }, {
+				 loader: "sass-loader"
+			 }]
+		 })
+	 }]
+ },
+ plugins: [
+	 extractSass
+ ]
+};
