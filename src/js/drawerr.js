56be4891d00584abaad7d68f58d3de244577c93e
Ferdie <ferdie@yard.nl>
2017-06-14 17:15:29 +0200

    Added menu icon styling

--- src/js/drawerr.js
+++ src/js/drawerr.js
@@ -2,18 +2,20 @@ var Drawerr = function(args) {
 	this.drawerr = document.querySelector(args.drawerr);
 	this.toggleBtn = document.querySelector(args.toggleBtn);
 	this.navbar = document.querySelector(args.navbar);
-	this.openClass = 'drawerr--open';
+	this.drawerOpenClass = 'drawerr--open';
+	this.toggleBtnActiveClass = 'drawerr-btn--active';
 };
 
 Drawerr.prototype = function() {
 	var init = function() {
 		this.drawerr.classList.add('drawerr');
+		this.toggleBtn.classList.add('drawerr-btn');
 		events( this );
 	},
 
 	events = function( self ) {
 		self.toggleBtn.addEventListener( 'click', function(e){
-			toggleClass(self);
+			toggleDrawer(self);
 		});
 
 		document.addEventListener( 'click', function(e) {
@@ -21,25 +23,30 @@ Drawerr.prototype = function() {
 		});
 	},
 
-	toggleClass = function(self) {
-		if( self.drawerr.classList.contains(self.openClass)) {
-			return self.drawerr.classList.remove(self.openClass);
-		} 
-		
-		self.drawerr.classList.add(self.openClass);
+	toggleDrawer = function(self) {
+		addOrRemoveClass( self.drawerr, self.drawerOpenClass );
+		addOrRemoveClass( self.toggleBtn, self.toggleBtnActiveClass );
 	},
+
+	addOrRemoveClass = function(el, addClass) {
+		if( el.classList.contains(addClass)) {
+			return el.classList.remove(addClass);
+		}
+
+		el.classList.add(addClass);
+	}
 
 	bodyClick = function(e,self) {
 		if( self.toggleBtn.contains(e.target )) return;
 
-		if( !self.drawerr.contains(e.target) && document.querySelector('.drawerr').classList.contains(self.openClass)) {
-			toggleClass(self);
+		if( !self.drawerr.contains(e.target) && document.querySelector('.drawerr').classList.contains(self.drawerOpenClass)) {
+			toggleDrawer(self);
+
 		}
 	}
 
 	return {
-		init:init,
-		toggleClass: toggleClass
+		init:init
 	}
 
 }();
