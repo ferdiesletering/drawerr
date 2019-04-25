b0a2d507b0200f4db1eb44ad5a939cf5cd43e58e
Ferdie Sletering <ferdie@yard.nl>
2019-04-25 21:24:36 +0200

    (chore): add eslint

--- src/js/drawerr.js
+++ src/js/drawerr.js
@@ -1,122 +1,123 @@
 const settings = {
-  bodyNoScrollClass : "drawerr-no-scroll",
-  drawerOpenClass  : "drawerr--open",
-  toggleBtnActiveClass :"drawerr-btn--active",
-  slideFromClass : "drawerr-slide-from-right",
+  bodyNoScrollClass: 'drawerr-no-scroll',
+  drawerOpenClass: 'drawerr--open',
+  toggleBtnActiveClass: 'drawerr-btn--active',
+  slideFromClass: 'drawerr-slide-from-right'
 }
 
 const options = {
-  btnText: "MENU",
-  drawerr: "#drawerr",
-  navbar: "header",
-  toggleBtn: ".toggleDrawerr",
-  slideFrom: "left",
+  btnText: 'MENU',
+  drawerr: '#drawerr',
+  navbar: 'header',
+  toggleBtn: '.toggleDrawerr',
+  slideFrom: 'left',
   preventBodyOnClick: false
 }
 
 export default class Drawerr {
-  constructor(args = {}) {
-    this.settings = settings;
-    this.options = Object.assign(options, args);
+  constructor (args = {}) {
+    this.settings = settings
+    this.options = Object.assign(options, args)
 
     // Events
-    this.openEvent = new Event("drawerr-open");
-    this.closeEvent = new Event("drawerr-close");
+    this.openEvent = new Event('drawerr-open')
+    this.closeEvent = new Event('drawerr-close')
 
     // Dom elements
-    this.body = document.querySelector("body");
-    this.drawerr = document.querySelector(this.options.drawerr);
-    this.toggleBtn = document.querySelector(this.options.toggleBtn);
-    this.navbar = document.querySelector(this.options.navbar);
+    this.body = document.querySelector('body')
+    this.drawerr = document.querySelector(this.options.drawerr)
+    this.toggleBtn = document.querySelector(this.options.toggleBtn)
+    this.navbar = document.querySelector(this.options.navbar)
 
-    this.beforeVisible();
+    this.beforeVisible()
 
     // Drawerr now visible
-    this.drawerr.classList.remove("drawerr--init");
-    this.drawerr.classList.add("drawerr");
-    this.toggleBtn.classList.add("drawerr-btn");
+    this.drawerr.classList.remove('drawerr--init')
+    this.drawerr.classList.add('drawerr')
+    this.toggleBtn.classList.add('drawerr-btn')
 
-    this.events();
+    this.events()
   }
 
-  beforeVisible() {
-    this.setSlideFromDirection();
-    this.drawerrOffsetTop();
-    this.insertHamburgerText();
+  beforeVisible () {
+    this.setSlideFromDirection()
+    this.drawerrOffsetTop()
+    this.insertHamburgerText()
   }
 
-  drawerrOffsetTop() {
-    this.drawerr.style.top = options.navbar.offsetHeight + "px";
+  drawerrOffsetTop () {
+    this.drawerr.style.top = options.navbar.offsetHeight + 'px';
   }
 
-  setSlideFromDirection() {
-    if (options.slideFrom === "right")
-      this.drawerr.classList.add(this.settings.slideFromClass);
+  setSlideFromDirection () {
+    if (options.slideFrom === 'right') {
+      this.drawerr.classList.add(this.settings.slideFromClass)
+    }
   }
 
-  events() {
-    this.toggleBtn.addEventListener("click", () => {
-      this.toggleDrawer();
-    });
+  events () {
+    this.toggleBtn.addEventListener('click', () => {
+      this.toggleDrawer()
+    })
 
-    document.addEventListener("click", e => {
-      this.bodyClick(e);
-    });
+    document.addEventListener('click', e => {
+      this.bodyClick(e)
+    })
 
     window.onresize = e => {
-      this.onResize();
+      this.onResize()
     };
   }
 
-  toggleDrawer() {
-    this.addOrRemoveClass(this.body, this.settings.bodyNoScrollClass);
-    this.addOrRemoveClass(this.drawerr, this.settings.drawerOpenClass );
-    this.addOrRemoveClass(this.toggleBtn, this.settings.toggleBtnActiveClass);
+  toggleDrawer () {
+    this.addOrRemoveClass(this.body, this.settings.bodyNoScrollClass)
+    this.addOrRemoveClass(this.drawerr, this.settings.drawerOpenClass)
+    this.addOrRemoveClass(this.toggleBtn, this.settings.toggleBtnActiveClass)
 
-    this.dispatchEvents();
+    this.dispatchEvents()
   }
 
-  onResize() {
-    this.drawerrOffsetTop();
+  onResize () {
+    this.drawerrOffsetTop()
   }
 
-  bodyClick(e) {
-    if ( this.toggleBtn.contains(e.target)) return;
+  bodyClick (e) {
+    if (this.toggleBtn.contains(e.target)) return
 
-    if ( this.options.preventBodyOnClick) return;
+    if (this.options.preventBodyOnClick) return
 
     if (
       !this.drawerr.contains(e.target) &&
       document
         .querySelector(options.drawerr)
-        .classList.contains(this.settings.drawerOpenClass )
+        .classList.contains(this.settings.drawerOpenClass)
     ) {
-      this.toggleDrawer();
+      this.toggleDrawer()
     }
   }
 
-  dispatchEvents() {
-    if (this.drawerr.classList.contains(this.settings.drawerOpenClass )) {
-      document.dispatchEvent(this.openEvent);
+  dispatchEvents () {
+    if (this.drawerr.classList.contains(this.settings.drawerOpenClass)) {
+      document.dispatchEvent(this.openEvent)
     } else {
-      document.dispatchEvent(this.closeEvent);
+      document.dispatchEvent(this.closeEvent)
     }
   }
 
-  addOrRemoveClass(el, addClass) {
+  addOrRemoveClass (el, addClass) {
     if (el.classList.contains(addClass)) {
-      return el.classList.remove(addClass);
+      return el.classList.remove(addClass)
     }
-    el.classList.add(addClass);
+    el.classList.add(addClass)
   }
 
-  insertHamburgerText() {
-    if (options.btnText.length === 0) return;
+  insertHamburgerText () {
+    if (options.btnText.length === 0) return
 
-    this.toggleBtn.classList.add("drawerr-btn--has-menu-text");
+    this.toggleBtn.classList.add('drawerr-btn--has-menu-text')
     this.toggleBtn.insertAdjacentHTML(
-      "beforeend",
+      'beforeend',
       `<span class="drawerr-btn__menu-text">${options.btnText}</span>`
-    );
+    )
   }
 }
--- src/js/drawerrMultilevel.js
+++ src/js/drawerrMultilevel.js
@@ -1,236 +1,258 @@
-import Drawerr from './drawerr';
+import Drawerr from './drawerr'
 
 const multilevelSettings = {
-	activeSubmenu: false,
-	submenus: false,
-	navigationTextClass: "drawerr-navigation",
-	hasSubmenuClass: "drawerr-item-has-submenu",
-	submenuClass: "drawerr-submenu",
-	submenuActiveClass: "drawerr-submenu--active",
-	navigationContainerClass: "drawerr-navigation-container",
-    hiddenClass: 'hidden',
-    showOnLoad: false,
-	noHashLinkClass: 'drawer-item-hashlink',
-    subMenuLinkClass: 'drawerr-submenu-link',
-    toggleBtnAriaLabel: 'navigation'
-};
+  activeSubmenu: false,
+  submenus: false,
+  navigationTextClass: 'drawerr-navigation',
+  hasSubmenuClass: 'drawerr-item-has-submenu',
+  submenuClass: 'drawerr-submenu',
+  submenuActiveClass: 'drawerr-submenu--active',
+  navigationContainerClass: 'drawerr-navigation-container',
+  hiddenClass: 'hidden',
+  showOnLoad: false,
+  noHashLinkClass: 'drawer-item-hashlink',
+  subMenuLinkClass: 'drawerr-submenu-link',
+  toggleBtnAriaLabel: 'navigation'
+}
 
 const multilevelOptions = {
-	navigationText: 'MENU'
-};
+  navigationText: 'MENU'
+}
 
 export default class DrawerrMultilevel extends Drawerr {
-	constructor(args) {
-		super(args);
+  constructor (args) {
+    super(args)
 
-		this.options.navigationText = args.navigationText || multilevelOptions.navigationText;
+    this.options.navigationText =
+      args.navigationText || multilevelOptions.navigationText
 
-		this.multilevelSettings = multilevelSettings;
-		this.drawerr.classList.add('drawerr-multilevel');
+    this.multilevelSettings = multilevelSettings
+    this.drawerr.classList.add('drawerr-multilevel')
 
-		// Setup navigation
-		this.insertNavigation();
-		this.navigation = document.querySelector(`.${this.multilevelSettings.navigationTextClass}`);
+    // Setup navigation
+    this.insertNavigation()
+    this.navigation = document.querySelector(
+      `.${this.multilevelSettings.navigationTextClass}`
+    )
 
-		this.navigationText = this.navigation.querySelector(
-			`.${this.multilevelSettings.navigationTextClass}__text`
-		);
-		this.navigationIcon = document.querySelector(
-			`.${this.multilevelSettings.navigationTextClass}__icon`
-		);
+    this.navigationText = this.navigation.querySelector(
+      `.${this.multilevelSettings.navigationTextClass}__text`
+    )
+    this.navigationIcon = document.querySelector(
+      `.${this.multilevelSettings.navigationTextClass}__icon`
+    )
 
-		// Submenu's
-		this.multilevelSettings.submenus = this.drawerr.querySelectorAll("ul li ul");
-		this.addClassToSubmenus();
+    // Submenu's
+    this.multilevelSettings.submenus = this.drawerr.querySelectorAll(
+      'ul li ul'
+    )
+    this.addClassToSubmenus()
 
-		// Events
-		this.bindLinks();
-		this.navigationOnClick();
-        this.bindOnClose();
+    // Events
+    this.bindLinks()
+    this.navigationOnClick()
+    this.bindOnClose()
 
-        this.toggleBtn.setAttribute('aria-expanded', 'false');
-        this.toggleBtn.setAttribute('aria-label', this.multilevelSettings.toggleBtnAriaLabel);
+    this.toggleBtn.setAttribute('aria-expanded', 'false')
+    this.toggleBtn.setAttribute(
+      'aria-label',
+      this.multilevelSettings.toggleBtnAriaLabel
+    )
 
-        if(this.options.showOnLoad) {
-            this.toggleDrawer();
-        }
+    if (this.options.showOnLoad) {
+      this.toggleDrawer()
+    }
+  }
+
+  toggleDrawer () {
+    super.toggleDrawer()
+
+    if (this.toggleBtn.classList.contains('drawerr-btn--active')) {
+      this.toggleBtn.setAttribute('aria-expanded', 'true')
+      this.drawerr.querySelector('ul a').focus()
+    } else {
+      this.toggleBtn.setAttribute('aria-expanded', 'false')
+      this.toggleBtn.focus()
+    }
+  }
+
+  insertNavigation () {
+    this.navigationContainer = document.querySelector(
+      `.${this.multilevelSettings.navigationContainerClass}`
+    )
+
+    if (this.navigationContainer === null) {
+      this.navigationContainer = this.drawerr.insertAdjacentHTML(
+        'afterbegin',
+        `<div class="${
+          this.multilevelSettings.navigationContainerClass
+        }"></div>`
+      )
+
+      this.navigationContainer = document.querySelector(
+        `.${this.multilevelSettings.navigationContainerClass}`
+      )
     }
 
-    toggleDrawer() {
-        super.toggleDrawer();
-
-        if( this.toggleBtn.classList.contains( 'drawerr-btn--active' ) ) {
-            this.toggleBtn.setAttribute('aria-expanded', 'true');
-            this.drawerr.querySelector('ul a').focus();
-        } else {
-            this.toggleBtn.setAttribute('aria-expanded', 'false');
-            this.toggleBtn.focus();
-        }
-    }
-
-	insertNavigation() {
-		this.navigationContainer = document.querySelector(`.${this.multilevelSettings.navigationContainerClass}`);
-
-		if (this.navigationContainer === null) {
-			this.navigationContainer = this.drawerr.insertAdjacentHTML(
-				"afterbegin",
-				`<div class="${this.multilevelSettings.navigationContainerClass}"></div>`
-			);
-
-            this.navigationContainer = document.querySelector(`.${this.multilevelSettings.navigationContainerClass}`);
-		}
-
-		this.navigationContainer.insertAdjacentHTML(
-			"afterbegin",
-			` <a aria-hidden="true" class="${this.multilevelSettings.navigationTextClass}" href="#"><span class="${
-      this.multilevelSettings.navigationTextClass
+    this.navigationContainer.insertAdjacentHTML(
+      'afterbegin',
+      ` <a aria-hidden="true" class="${
+        this.multilevelSettings.navigationTextClass
+      }" href="#"><span class="${
+        this.multilevelSettings.navigationTextClass
       }__icon ${this.multilevelSettings.hiddenClass}"></span><span class="${
-      this.multilevelSettings.navigationTextClass
+        this.multilevelSettings.navigationTextClass
       }__text">${this.options.navigationText}</span></a>`
-		);
-	}
+    )
+  }
 
-	addClassToSubmenus() {
-		Array.prototype.forEach.call(this.multilevelSettings.submenus, menu => {
-			menu.classList.add(this.multilevelSettings.submenuClass);
-		});
-	}
+  addClassToSubmenus () {
+    Array.prototype.forEach.call(this.multilevelSettings.submenus, menu => {
+      menu.classList.add(this.multilevelSettings.submenuClass)
+    })
+  }
 
-	reset() {
-		setTimeout(() => {
-			Array.prototype.forEach.call(this.multilevelSettings.submenus, menu => {
-				menu.classList.remove(this.multilevelSettings.submenuActiveClass);
-			});
+  reset () {
+    setTimeout(() => {
+      Array.prototype.forEach.call(this.multilevelSettings.submenus, menu => {
+        menu.classList.remove(this.multilevelSettings.submenuActiveClass)
+      })
 
-			this.setNavigationText(this.options.navigationText);
-			this.hideShowNavigationIcon();
-		}, 300);
-	}
+      this.setNavigationText(this.options.navigationText)
+      this.hideShowNavigationIcon()
+    }, 300)
+  }
 
-	bindOnClose() {
-        document.addEventListener('drawerr-close', this.reset.bind(this));
-        document.addEventListener('keydown', (e) => {
-            if(e.keyCode == 27) {
-                if( this.drawerr.classList.contains('drawerr--open')) {
-                    this.toggleDrawer();
-                }
-            }
-        });
-	}
+  bindOnClose () {
+    document.addEventListener('drawerr-close', this.reset.bind(this))
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === 27) {
+        if (this.drawerr.classList.contains('drawerr--open')) {
+          this.toggleDrawer()
+        }
+      }
+    })
+  }
 
-	bindLinks() {
-		const links = this.drawerr.querySelectorAll("ul a");
+  bindLinks () {
+    const links = this.drawerr.querySelectorAll('ul a')
 
-		if (links.length) {
-			Array.prototype.forEach.call(links, link => {
-				const parent = link.parentElement;
+    if (links.length) {
+      Array.prototype.forEach.call(links, link => {
+        const parent = link.parentElement
 
-				if (parent.children.item(parent.children.length - 1).classList.contains(this.multilevelSettings.submenuClass)) {
-					link.classList.add(this.multilevelSettings.hasSubmenuClass);
+        if (
+          parent.children
+            .item(parent.children.length - 1)
+            .classList.contains(this.multilevelSettings.submenuClass)
+        ) {
+          link.classList.add(this.multilevelSettings.hasSubmenuClass)
 
-					if (link.getAttribute('href') !== '#') {
-						this.addSubmenuLink(link);
-					}
-				}
+          if (link.getAttribute('href') !== '#') {
+            this.addSubmenuLink(link)
+          }
+        }
 
-				link.addEventListener("click", this.linkOnClick.bind(this));
-			});
-		}
-	}
+        link.addEventListener('click', this.linkOnClick.bind(this))
+      })
+    }
+  }
 
-	/**
-	 * Add submenu link to existing link so the original and the submenu can be clicked
-	 *
-	 * @param {*} link
-	 */
-	addSubmenuLink(link) {
+  /**
+   * Add submenu link to existing link so the original and the submenu can be clicked
+   *
+   * @param {*} link
+   */
+  addSubmenuLink (link) {
+    const submenuLink = document.createElement('a')
 
-		const submenuLink = document.createElement('a');
+    link.parentElement.classList.add(this.multilevelSettings.noHashLinkClass)
+    submenuLink.setAttribute('href', '#')
+    submenuLink.setAttribute('class', this.multilevelSettings.subMenuLinkClass)
 
-		link.parentElement.classList.add(this.multilevelSettings.noHashLinkClass);
-		submenuLink.setAttribute('href', '#');
-        submenuLink.setAttribute('class', this.multilevelSettings.subMenuLinkClass);
+    link.insertAdjacentElement('afterend', submenuLink)
 
-		link.insertAdjacentElement(
-			"afterend",
-			submenuLink
-		);
+    submenuLink.addEventListener('click', this.linkOnClick.bind(this))
+  }
 
-		submenuLink.addEventListener("click", this.linkOnClick.bind(this));
-	}
+  /**
+   *
+   * @param {*} e
+   */
+  linkOnClick (e) {
+    const link = e.target
+    const submenu = link.parentElement.querySelector(
+      `.${this.multilevelSettings.submenuClass}`
+    )
+    let submenuLink = false
+    let breadcrumbText = ''
 
-	/**
-	 *
-	 * @param {*} e
-	 */
-	linkOnClick(e) {
+    if (link.classList.contains(this.multilevelSettings.subMenuLinkClass)) {
+      // Submenu item has no link but we need to set the breadcrumb text
+      submenuLink = link.previousSibling
+    }
 
-		const link = e.target;
-		const submenu = link.parentElement.querySelector(
-			`.${this.multilevelSettings.submenuClass}`
-		);
-		let submenuLink = false;
-		let breadcrumbText = '';
+    if (submenu !== null) {
+      if (!submenuLink) {
+        breadcrumbText = link.textContent
 
-		if (link.classList.contains(this.multilevelSettings.subMenuLinkClass)) {
-			// Submenu item has no link but we need to set the breadcrumb text
-			submenuLink = link.previousSibling;
-		}
+        // Go to the clicked link url instead of navigating the menu
+        if (link.getAttribute('href') !== '#') {
+          return false
+        }
+      } else {
+        breadcrumbText = submenuLink.textContent
+      }
 
-		if (submenu !== null) {
+      submenu.classList.add(this.multilevelSettings.submenuActiveClass)
+      this.setNavigationText(breadcrumbText)
+      this.multilevelSettings.activeSubmenu = submenu
+      this.hideShowNavigationIcon('show')
+    }
+  }
 
-			if (!submenuLink) {
-				breadcrumbText = link.textContent;
+  navigationOnClick () {
+    this.navigation.addEventListener('click', () => {
+      if (!this.multilevelSettings.activeSubmenu) return
 
-				// Go to the clicked link url instead of navigating the menu
-				if (link.getAttribute('href') !== '#') {
-					return false;
-				}
-			} else {
-				breadcrumbText = submenuLink.textContent;
-			}
+      this.multilevelSettings.activeSubmenu.classList.remove(
+        this.multilevelSettings.submenuActiveClass
+      )
+      this.multilevelSettings.activeSubmenu = this.multilevelSettings.activeSubmenu.parentElement.parentElement
 
-			submenu.classList.add(this.multilevelSettings.submenuActiveClass);
-			this.setNavigationText(breadcrumbText);
-			this.multilevelSettings.activeSubmenu = submenu;
-			this.hideShowNavigationIcon("show");
-
-		}
-	}
-
-	navigationOnClick() {
-		this.navigation.addEventListener("click", () => {
-			if (!this.multilevelSettings.activeSubmenu) return;
-
-			this.multilevelSettings.activeSubmenu.classList.remove(this.multilevelSettings.submenuActiveClass);
-			this.multilevelSettings.activeSubmenu = this.multilevelSettings.activeSubmenu.parentElement.parentElement;
-
-			if (!this.multilevelSettings.activeSubmenu.classList.contains("drawerr-submenu--active")) {
-				this.hideShowNavigationIcon(this.multilevelSettings.hiddenClass);
-				this.setNavigationText(this.options.navigationText);
-			} else {
-				this.setNavigationText(
-					this.multilevelSettings.activeSubmenu.parentElement.querySelector("a").textContent
-				);
-			}
-		});
-	}
+      if (
+        !this.multilevelSettings.activeSubmenu.classList.contains(
+          'drawerr-submenu--active'
+        )
+      ) {
+        this.hideShowNavigationIcon(this.multilevelSettings.hiddenClass)
+        this.setNavigationText(this.options.navigationText)
+      } else {
+        this.setNavigationText(
+          this.multilevelSettings.activeSubmenu.parentElement.querySelector('a')
+            .textContent
+        )
+      }
+    })
+  }
 
   /**
    *
    * @param {*} text
    */
-	setNavigationText(text) {
-		this.navigationText.textContent = text;
-	}
+  setNavigationText (text) {
+    this.navigationText.textContent = text
+  }
 
   /**
    *
    * @param {*} action
    */
-	hideShowNavigationIcon(action) {
-		action == "show" ?
-			this.navigationIcon.classList.remove(this.multilevelSettings.hiddenClass) :
-			this.navigationIcon.classList.add(this.multilevelSettings.hiddenClass);
-	}
+  hideShowNavigationIcon (action) {
+    action === 'show'
+      ? this.navigationIcon.classList.remove(
+        this.multilevelSettings.hiddenClass
+      )
+      : this.navigationIcon.classList.add(this.multilevelSettings.hiddenClass)
+  }
 }
