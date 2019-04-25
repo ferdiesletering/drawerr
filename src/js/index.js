7a949dc2a5dd9eae7c19a3236f86b6ded88acfee
Ferdie Sletering <ferdie@yard.nl>
2019-04-25 21:12:50 +0200

    (fix): Prevent double scrollbars when menu height exceeds browser window height close #21

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,13 @@
  * We need this main function to correctly exports drawerr
  * see http://siawyoung.com/coding/javascript/exporting-es6-modules-as-single-scripts-with-webpack.html
  */
-import Drawerr from './drawerr';
-import DrawerrMultilevel from 'drawerr';
+import Drawerr from "./drawerr";
+import DrawerrMultilevel from "./drawerrMultilevel";
 
 export default {
-    Drawerr,
-    DrawerrMultilevel
-}
+  Drawerr,
+  DrawerrMultilevel
+};
 
 window.drawerr = Drawerr;
 window.drawerrMultilevel = DrawerrMultilevel;
